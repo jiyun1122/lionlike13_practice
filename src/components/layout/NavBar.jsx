2b3bd1262edb9e65713118644e742f0baeb1f0b4
chaeyoungwon <epopcy@naver.com>
2025-07-16 23:59:59 +0900

    feat: 소셜로그인 실습용 추가 코드 및 주석 작성

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,29 +1,44 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useAuthStore } from '../../stores/useAuthStore';
+import { LoginModal } from '../modal/LoginModal';
 
 export const NavBar = () => {
-  return (
-    <nav className="fixed top-0 border-b-[1.5px] border-main left-0 right-0 h-20 text-black bg-white flex items-center justify-end px-10 z-50">
-      <Link
-        to="/"
-        className="absolute left-1/2 -translate-x-1/2 font-semibold text-2xl text-main"
-      >
-        HONGIK LIKELION
-      </Link>
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-      <div className="flex gap-4">
+  const handleMyPageClick = () => {
+    // TODO: 로그인 여부 가져오기
+    navigate('/mypage');
+  };
+
+  return (
+    <>
+      <nav className="fixed top-0 border-b-[1.5px] border-main left-0 right-0 h-20 text-black bg-white flex items-center justify-end px-10 z-50">
         <Link
-          to="/login"
-          className="bg-main text-white font-semibold rounded-sm px-5 py-2 hover:bg-[#3a35a0] transition"
+          to="/"
+          className="absolute left-1/2 -translate-x-1/2 font-semibold text-2xl text-main"
         >
-          Login
+          HONGIK LIKELION
         </Link>
-        <Link
-          to="/mypage"
-          className="bg-main text-white font-semibold rounded-sm px-5 py-2 hover:bg-[#3a35a0] transition"
-        >
-          My Page
-        </Link>
-      </div>
-    </nav>
+
+        <div className="flex gap-4">
+          <Link
+            to="/login"
+            className="bg-main text-white font-semibold rounded-sm px-5 py-2 hover:bg-[#3a35a0] transition"
+          >
+            Login
+          </Link>
+          <button
+            onClick={handleMyPageClick}
+            className="bg-main text-white font-semibold rounded-sm px-5 py-2 hover:bg-[#3a35a0] transition"
+          >
+            My Page
+          </button>
+        </div>
+      </nav>
+      {/* 로그인 모달 띄우기 */}
+    </>
   );
 };
