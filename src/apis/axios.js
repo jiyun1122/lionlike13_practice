3cf1cb8da70e63d8973141e7d6c3b8e22568c4e1
chaeyoungwon <epopcy@naver.com>
2025-07-17 00:15:00 +0900

    fix: export 추가

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../stores/useAuthStore';
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: 'https://api.example.com', // 백엔드 URL .env로
   headers: {
     'Content-Type': 'application/json',
