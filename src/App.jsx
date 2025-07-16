7208874b4fd265e13f691c07ef06556927d80942
chaeyoungwon <epopcy@naver.com>
2025-07-16 14:13:17 +0900

    feat: 소셜로그인 실습용 코드 추가

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Login } from './pages/Login';
+import { Home } from './pages/Home';
+import { MyPage } from './pages/MyPage';
+import { NotFound } from './pages/NotFound';
+
+import { Layout } from './components/layout/Layout';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
